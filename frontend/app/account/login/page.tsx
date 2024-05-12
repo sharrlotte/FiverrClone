ccd9f3badb94e2cdedaab4dc6588d472e0356964
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-05-12 16:29:46 +0700

    add-login-page

--- frontend/app/account/login/page.tsx
+++ frontend/app/account/login/page.tsx
@@ -7,40 +7,53 @@ import { Key } from 'lucide-react';
 type Post = {
 	link: string;
 	artist: string;
+	alt: string;
 };
 
 const images: Post[] = [
-	{ link: '/image/facebook-icon.ico', artist: 'Continiue with facebook' },
-	{ link: '/image/google-icon.ico', artist: 'Continiue with google' },
+	{ link: '/image/facebook-icon.ico', alt: 'facebook', artist: 'Continiue with facebook' },
+	{ link: '/image/google-icon.ico', alt: 'google', artist: 'Continiue with google' },
 ];
 
 export default function Page() {
 	return (
 		<main className='h-full w-full flex justify-center items-center'>
-			<div className='flex flex-row border h-3/5 w-3/5 shadow-2xl justify-between'>
-				<div>
-					ben phai
+			<div className='flex flex-row border h-3/5 w-3/5 shadow-2xl rounded-lg '>
+				<div className='flex flex-col pl-20 gap-5 w-1/2 bg-rose-600'>
+					<h1 className='text-nowrap font-black mt-10 text-white'>Success starts here</h1>
+					<ul className='text-white list-image-none'>
+						<li>
+							<span>Over 600 categories</span>
+						</li>
+						<li>
+							<span>Pay per project, not per hour</span>
+						</li>
+						<li>
+							<span>Access to talent and businesses across the globe</span>
+						</li>
+					</ul>
 				</div>
-				<div className='flex flex-col px-44 gap-5 justify-center'>
-					<div className='flex flex-col'>
+				<div className='flex flex-col px-20 w-1/2 bg-white'>
+					<div className='flex flex-col gap-2 mt-10'>
 						<span className='font-black'>Create a new account</span>
-						<small>Already have an account? <a className='underline' href="">Sign in</a></small>
+						<span className='text-xs'>Already have an account? <a className='underline' href="">Sign in</a></span>
 					</div>
-					<div className='flex flex-col gap-4'>
-						{images.map(({ link, artist }, index) => (
+					<div className='flex flex-col gap-4 mt-5'>
+						{images.map(({ link, alt, artist }, index) => (
 							<div
 								key={index}
-								className='flex flex-row'
 							>
 
-								<Button variant='outline'>
-									<div className='flex flex-row gap-4'>
+								<Button variant='outline'
+									className='w-full rounded'>
+									<div className='flex flex-row gap-2'>
 										<Image
 											src={link}
-											alt='error 304'
-											height={30}
-											width={30}
-										/>{artist}
+											alt={alt}
+											height={20}
+											width={20}
+										/>
+										{artist}
 									</div>
 								</Button>
 							</div>

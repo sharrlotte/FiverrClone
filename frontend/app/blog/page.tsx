0ca101e6f2c61b34cb9fd6098694618ebd95a802
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-05-15 15:10:13 +0700

    fix error

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
 import MarkdownIt from 'markdown-it';
-import MdEditor from 'react-markdown-editor-lite';
+import React from 'react';
 import 'react-markdown-editor-lite/lib/index.css';
+import MdEditor from 'react-markdown-editor-lite';
 
 // Initialize a markdown parser
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
 // Finish!
-function handleEditorChange({ html, text }: { html: string, text: string }) {
-    console.log('handleEditorChange', html, text);
+function handleEditorChange({ html, text }: { html: string; text: string }) {
+	console.log('handleEditorChange', html, text);
 }
 
 // Định nghĩa kiểu dữ liệu cho props là kiểu string
 type YourPropsType = {
-    yourPropName: string;
-}
+	yourPropName: string;
+};
 
 const YourComponent: React.FC<YourPropsType> = (props) => {
-    // Sử dụng props ở đây
-    return (
-        <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
-    );
+	// Sử dụng props ở đây
+	return (
+		<MdEditor
+			style={{ height: '500px' }}
+			renderHTML={(text: string) => mdParser.render(text)}
+			onChange={handleEditorChange}
+		/>
+	);
 };
 
 export default YourComponent;

c07980060ecb8282dd519d0b0a774e4f17291511
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-05-17 17:08:57 +0700

    blog-page

---
+++ frontend/components/ui/markdown.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { cn } from '@/lib/utils';
+import rehypeSanitize from 'rehype-sanitize';
+import env from '@/app/constant/env';
+import Image from 'next/image';
+interface MarkdownProps {
+    className?: string;
+    children: string;
+}
+
+function RouterLink({ href, children }: any) {
+    return href.match(/^(https?:)?\/\//) ? (
+        <a
+            className="text-emerald-500"
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+        >
+            {children}
+        </a>
+    ) : (
+        <Link href={href}>{children}</Link>
+    );
+}
+
+function MarkdownImage({ src, alt }: any) {
+    if (src && src.includes(env.url.base)) {
+        src = 'blob:' + src;
+    }
+
+    return (
+        <Image
+            className="markdown-image h-full w-full"
+            alt={alt}
+            src={src}
+            width={1024}
+            height={800}
+            loader={({ src }) => `${src}`}
+        />
+    );
+}
+
+export default function Markdown({ className, children }: MarkdownProps) {
+    return (
+        <ReactMarkdown
+            className={cn('markdown', className)}
+            components={{ a: RouterLink, img: MarkdownImage }}
+            rehypePlugins={[rehypeSanitize]}
+        >
+            {children}
+        </ReactMarkdown>
+    );
+}

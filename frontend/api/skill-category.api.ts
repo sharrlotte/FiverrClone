ab74a2c2e976d4de18c7822d8747af8f8dcaf9f7
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-05-21 18:33:53 +0700

    fix skill category table

--- frontend/api/skill-category.api.ts
+++ frontend/api/skill-category.api.ts
@@ -1,8 +1,5 @@
-import {
-  CreateSkillCategoryRequest,
-  GetSkillCategoryRequest,
-} from "@/schema/skill-category.schema";
-import api from "./api";
+import { CreateSkillCategoryRequest, GetSkillCategoryRequest } from '@/schema/skill-category.schema';
+import api from './api';
 
 export type SkillCategory = {
   id: number;
@@ -12,13 +9,13 @@ export type SkillCategory = {
 };
 
 export async function createSkillCategory(request: CreateSkillCategoryRequest) {
-  return api.post("/skill-categories", request, {
+  return api.post('/skill-categories', request, {
     data: request,
   });
 }
 
-export async function getSkillCategory(
-  request: GetSkillCategoryRequest
-): Promise<SkillCategory[]> {
-  return api.get("/skill-categories", { params: request });
+export async function getSkillCategory(request: GetSkillCategoryRequest): Promise<SkillCategory[]> {
+  const result = await api.get('/skill-categories', { params: request });
+
+  return result.data;
 }

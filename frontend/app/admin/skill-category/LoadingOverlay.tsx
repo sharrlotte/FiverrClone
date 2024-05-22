f945912c5b850cf1f41330eeeea4844cea469ba2
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-05-23 01:17:40 +0700

    fix LoadingOverlay

--- frontend/app/admin/skill-category/LoadingOverlay.tsx
+++ frontend/app/admin/skill-category/LoadingOverlay.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function LoadingOverlay() {
   return (
     <div className="fixed h-full w-full top-0 left-0 justify-center flex items-center brightness-50 bg-black/50 z-[9999] overflow-hidden">
-      <Image alt="Loading icon" src="/image/loading.svg" />
+      <Image alt="Loading icon" src="/image/loading.svg" fill />
     </div>
   );
 }

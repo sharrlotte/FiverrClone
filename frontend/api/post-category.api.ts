1389f99ca08963bf8ae266f651446af8a47ee3f2
Dat-upoverloading <trandinhdat003@gmail.com>
2024-05-24 17:44:58 +0700

    post category

---
+++ frontend/api/post-category.api.ts
@@ -0,0 +1,32 @@
+import { CreatePostCategoryRequest, GetPostCategoryRequest } from '@/schema/post-category.schema';
+import api from './api';
+
+export type PostCategory = {
+  id: number;
+  name: string;
+  description: string;
+  parentId: number | null;
+  createdAt: Date;
+};
+
+export async function createPostCategory(request: CreatePostCategoryRequest) {
+  return api.post('/post-categories', request, {
+    data: request,
+  });
+}
+
+export async function getPostCategory(request: GetPostCategoryRequest): Promise<PostCategory[]> {
+  const result = await api.get('/post-categories', { params: request });
+
+  return result.data;
+}
+
+export async function updaterPostCategory(id: number, request: CreatePostCategoryRequest) {
+  return api.patch(`/post-categories/${id}`, request, {
+    data: request,
+  });
+}
+
+export async function deletePostCategory(id: number) {
+  return api.delete(`/post-categories/${id}`);
+}
---
+++ frontend/app/admin/post-category/AddPostCategoryButton.tsx
@@ -0,0 +1,138 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { useToast } from '@/components/ui/use-toast';
+import { CreatePostCategoryRequest, createPostCategorySchema } from '@/schema/post-category.schema';
+import { createPostCategory } from '@/api/post-category.api';
+import LoadingOverlay from '../../../components/ui/LoadingOverlay';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { PlusCircleIcon } from '@heroicons/react/24/outline';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+export default function AddPostCategoryButton() {
+  const queryClient = useQueryClient();
+  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+
+  const form = useForm<CreatePostCategoryRequest>({
+    resolver: zodResolver(createPostCategorySchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      parentId: null,
+    },
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (value: CreatePostCategoryRequest) => createPostCategory(value),
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+      form.reset();
+    },
+    onError: (error: any) => {
+      switch (error.response.status) {
+        case 409:
+          toast({
+            title: 'Lỗi',
+            description: 'Tên thể loại đã tồn tại, vui lòng chọn tên khác',
+            variant: 'destructive',
+          });
+          break;
+
+        default:
+          toast({
+            title: 'Lỗi',
+            description: 'Có lỗi đã xảy ra, vui lòng thử lại sau',
+            variant: 'destructive',
+          });
+          break;
+      }
+    },
+    onSettled: () => {
+      setOpen(false);
+    },
+  });
+
+  return (
+    <div>
+      {isPending && <LoadingOverlay />}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="items-center gap-2 h-10" variant="outline">
+            <PlusCircleIcon className="w-6 h-6" />
+            <span>Thêm</span>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="overflow-auto h-full">
+          <Form {...form}>
+            <h3 className="text-xl font-semibold">Thêm thể loại bài viết</h3>
+            <form onSubmit={form.handleSubmit((data) => mutate(data))} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tên thể loại</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Tên" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Mô tả thể loại</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Mô tả" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="parentId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Thể loại cha</FormLabel>
+                    <FormControl>
+                      <Select>
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Chọn thể loại" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectLabel>Fruits</SelectLabel>
+
+                            <SelectItem value="apple">Apple</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="w-full flex justify-end">
+                <Button type="submit">Lưu</Button>
+              </div>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}

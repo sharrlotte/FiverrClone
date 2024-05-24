a311d0dc890bc9b7b96b809129c100dbf667955e
Dat-upoverloading <trandinhdat003@gmail.com>
2024-05-24 16:45:42 +0700

    tag category

---
+++ frontend/app/admin/tag/DeleteTagButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DeleteTagButton() {
+  return (
+    <div>DeleteTagButton</div>
+  )
+}
---
+++ frontend/app/admin/tag/UpdateTagButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UpdateTagButton() {
+  return (
+    <div>UpdateTagButton</div>
+  )
+}

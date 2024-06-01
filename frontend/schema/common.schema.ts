6ad4a97875926127dbdbf52a24e0a6cca119b2e1
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-06-01 20:00:32 +0700

    Fix File not defined

--- frontend/schema/common.schema.ts
+++ frontend/schema/common.schema.ts
@@ -5,13 +5,13 @@ import { z } from 'zod';
 export const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
 
 export const FileSchema = z
-  .instanceof(File, { message: 'Ảnh chưa được chọn' })
+  .any()
   .refine((files) => files !== undefined)
   .refine((file) => !!file, 'Image is required.')
   .refine((file) => (file ? ACCEPTED_IMAGE_TYPES.includes(file.type) : false), '.jpg, .jpeg, .png files are accepted.');
 
 export const FileListSchema = z
-  .instanceof(FileList, { message: 'Ảnh chưa được chọn' })
+  .any()
   .refine((files) => files !== undefined)
   .refine((files) => !!files, 'Image is required.')
   .transform((files) =>

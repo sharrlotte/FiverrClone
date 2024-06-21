f1f54199de86016397e147fa85bf2e39ee5e8ca7
Dat-upoverloading <trandinhdat003@gmail.com>
2024-06-21 16:20:12 +0700

    comiy

--- frontend/app/(everyone)/layout.tsx
+++ frontend/app/(everyone)/layout.tsx
@@ -1,39 +1,11 @@
-'use client';
 import Header from '@/app/Header';
-import ProtectedRoute from '@/components/layout/protected-route';
-import React, { ReactNode, useEffect } from 'react';
-import { useSession } from '@/context/SessionContext';
-import { useRouter } from 'next/navigation';
+import React, { ReactNode } from 'react';
 
-const checkFirstVisit = () => {
-  if (typeof window !== 'undefined') {
-    const hasVisited = localStorage.getItem('hasVisited');
-    if (!hasVisited) {
-      localStorage.setItem('hasVisited', 'true');
-      return true;
-    }
-    return false;
-  }
-  return false;
-};
-
-export default function Layout({ children }: { children: ReactNode }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (checkFirstVisit()) {
-      router.replace('/promopage');
-    }
-  }, []);
-
-  const { session } = useSession();
-
+export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <ProtectedRoute session={session} all={['USER']}>
-      <div className="h-full flex flex-col px-4">
-        <Header className="p-4" />
-        {children}
-      </div>
-    </ProtectedRoute>
+    <div className="h-full flex flex-col px-4">
+      <Header className="p-4" />
+      {children}
+    </div>
   );
 }

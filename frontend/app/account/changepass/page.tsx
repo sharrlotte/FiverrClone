124fb399c14ce04d62f2624b3ffcb38e4a95595e
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-06-27 16:32:06 +0700

    change pass page

--- frontend/app/account/changepass/page.tsx
+++ frontend/app/account/changepass/page.tsx
@@ -1,46 +1,47 @@
-import * as React from "react"
-import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+'use client';
 
-export default function Page() {
+import React, { useState } from "react";
+import 'tailwindcss/tailwind.css';
+
+const LoginRegister: React.FC = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleRegisterClick = () => {
+        setIsActive(true);
+    };
+
+    const handleLoginClick = () => {
+        setIsActive(false);
+    };
+
     return (
-        <main className="h-full w-full flex justify-center items-center">
-            <div className="flex lg:flex-row lg:h-3/5 w-full h-full flex-col lg:w-3/5 shadow-2xl rounded-lg overflow-hidden border">
-                <Card className="w-full">
-                    <CardHeader>
-                        <CardTitle className="text-7xl">Đổi mật khẩu</CardTitle>
-                        <CardDescription>Deploy your new project in one-click.</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <form>
-                            <div className="grid w-full items-center gap-4">
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="name">Name</Label>
-                                    <Input id="name" placeholder="Name of your project" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="framework">Framework</Label>
-                                    <Input id="name" placeholder="Name of your project" />
-                                </div>
-                            </div>
-                        </form>
-                    </CardContent>
-                    <CardFooter className="flex justify-between">
-                        <Button variant="outline">Cancel</Button>
-                        <Button>Deploy</Button>
-                    </CardFooter>
-                </Card>
+        <div className="flex items-center justify-center h-screen bg-gradient-to-r from-gray-300 to-blue-200">
+            <div id="container" className={`relative w-full max-w-3xl min-h-[500px] bg-white rounded-2xl shadow-lg overflow-hidden ${isActive ? 'active' : ''}`}>
+                <div>
+                </div>
+                <div className="flex justify-center items-center mt-36 ">
+                    <form className="flex flex-col items-center justify-center h-full w-96 gap-5">
+                        <h1 className="font-bold text-5xl mb-5">Đổi mật khẩu</h1>
+                        <span className="text-sm mb-3 text-nowrap">Hãy đổi mật khẩu của bạn</span>
+                        <input type="text" placeholder="Mật khẩu cũ" className="w-full px-3 py-3 mt-3 text-sm bg-gray-200 rounded" />
+                        <input type="password" placeholder="Mật Khẩu mới" className="w-full px-3 py-2 mt-3 text-sm bg-gray-200 rounded" />
+                        <input type="password" placeholder="Nhập lại mật khẩu mới" className="w-full px-3 py-3 mt-3 text-sm bg-gray-200 rounded" />
+                        <div className="flex flex-row gap-20 mt-5 ">
+                            <button type="button" className="mt-4 px-6 py-2 text-sm font-semibold text-white bg-blue-600 rounded uppercase" onClick={handleLoginClick}>
+                                QUAY LAI
+                            </button>
+                            <button type="button" className="mt-4 px-6 py-2 text-sm font-semibold text-white bg-blue-600 rounded uppercase" onClick={handleLoginClick}>
+                                đồng ý
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </main>
+        </div>
+    );
+};
 
-    )
-}
+export default LoginRegister;
+
+
+

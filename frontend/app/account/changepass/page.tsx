f5b73ea562ff6e7c0c5b3390712094278ab7ac69
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-06-27 17:12:02 +0700

    last change

--- frontend/app/account/changepass/page.tsx
+++ frontend/app/account/changepass/page.tsx
@@ -19,25 +19,26 @@ const LoginRegister: React.FC = () => {
             <div id="container" className={`relative w-full max-w-3xl min-h-[500px] bg-white rounded-2xl shadow-lg overflow-hidden ${isActive ? 'active' : ''}`}>
                 <div>
                 </div>
-                <div className="flex justify-center items-center mt-36 ">
-                    <form className="flex flex-col items-center justify-center h-full w-96 gap-5">
+                <div className="flex justify-center items-center mt-24 ">
+                    <form className="flex flex-col items-center justify-center h-full w-9/12 gap-5">
                         <h1 className="font-bold text-5xl mb-5">Đổi mật khẩu</h1>
                         <span className="text-sm mb-3 text-nowrap">Hãy đổi mật khẩu của bạn</span>
                         <input type="text" placeholder="Mật khẩu cũ" className="w-full px-3 py-3 mt-3 text-sm bg-gray-200 rounded" />
                         <input type="password" placeholder="Mật Khẩu mới" className="w-full px-3 py-2 mt-3 text-sm bg-gray-200 rounded" />
                         <input type="password" placeholder="Nhập lại mật khẩu mới" className="w-full px-3 py-3 mt-3 text-sm bg-gray-200 rounded" />
-                        <div className="flex flex-row gap-20 mt-5 ">
-                            <button type="button" className="mt-4 px-6 py-2 text-sm font-semibold text-white bg-blue-600 rounded uppercase" onClick={handleLoginClick}>
-                                QUAY LAI
-                            </button>
+                        <div className="flex flex-row gap-48">
+                            <li className="mt-4 px-6 py-2 text-sm font-semibold text-blue-600 rounded uppercase list-none" onClick={handleLoginClick}>
+                                <a href=""> QUAY LAI</a>
+                            </li>
                             <button type="button" className="mt-4 px-6 py-2 text-sm font-semibold text-white bg-blue-600 rounded uppercase" onClick={handleLoginClick}>
                                 đồng ý
                             </button>
                         </div>
+
                     </form>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 

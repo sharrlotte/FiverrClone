cac2af92acfa49e9f28c676471211b2f9486aa28
vuleducthuan <vuleducthuan@gmail.com>
2024-06-30 12:37:56 +0700

    verify

--- frontend/app/account/verify/page.tsx
+++ frontend/app/account/verify/page.tsx
@@ -1,11 +1,9 @@
 'use client';
-
 import React, { useState } from "react";
 import 'tailwindcss/tailwind.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGooglePlusG, faFacebookF, faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
- 
 const LoginRegister: React.FC = () => {
     const [isActive, setIsActive] = useState(false);
 
@@ -16,18 +14,16 @@ const LoginRegister: React.FC = () => {
     const handleLoginClick = () => {
         setIsActive(false);
     };
-
     return (
         <div className="flex items-center justify-center h-screen bg-gradient-to-r from-gray-300 to-blue-200">
           <div id="container" className={`relative w-full max-w-3xl min-h-[500px] bg-white rounded-2xl shadow-lg overflow-hidden ${isActive ? 'active' : ''}`}>
               <div className="flex justify-center items-center mt-24">
                 <form className="flex flex-col items-center justify-center h-full w-8/12">
-                          <h1 className="font-bold text-4xl mb-5">Verify account</h1>
-                          <span className="text-1xl mb-4 ">Vui lòng xác nhận địa chỉ e-mail của bạn và nhập mã xác minh mà chung tôi đã gửi cho bạn.</span>
+                          <h1 className="font-bold text-4xl mb-5">Xác nhận tài khoản</h1>
+                          <span className="text-1xl mb-2 ">Vui lòng xác nhận địa chỉ e-mail của bạn và nhập mã </span>
+                          <span>xác minh mà chúng tôi đã gửi cho bạn.</span>
                           <div className="w-96">
-                            <span className="font-bold text-1xl flex">E-mail</span>
-                            <input type="email" placeholder="email@gmail.com" className="w-full px-2 py-2 mt-3 text-sm bg-gray-200 rounded" />
-                            <span className="font-bold text-1xl flex mt-3">Mã xác nhận </span>
+                            <span className="font-bold text-1xl flex mt-5">Mã xác nhận </span>
                             <InputOTP maxLength={6}>
                                 <InputOTPGroup>
                                   <InputOTPSlot index={0} />
@@ -38,14 +34,10 @@ const LoginRegister: React.FC = () => {
                                   <InputOTPSlot index={5} />
                                 </InputOTPGroup>
                             </InputOTP>
-                    
                           </div>
                           <div className="flex flex-row">
                             <div className="flex flex-row gap-5">
-                            <button type="button" className="mt-20 px-6 py-2 text-sm font-semibold text-white bg-purple-600 rounded uppercase" onClick={handleLoginClick}>
-                                Mã gửi lại
-                            </button>
-                          <button type="button" className="mt-20 px-6 py-2 text-sm font-semibold text-white bg-purple-600 rounded uppercase" onClick={handleLoginClick}>
+                          <button type="button" className="mt-10 px-6 py-2 text-sm font-semibold text-white bg-purple-600 rounded uppercase" onClick={handleLoginClick}>
                                 Mã xác nhận 
                           </button>
                             </div>

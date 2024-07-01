6b6dbef96e590b49837c3962862ded7c06d891ae
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-07-01 14:45:30 +0700

    login page

--- frontend/app/account/login/page.tsx
+++ frontend/app/account/login/page.tsx
@@ -4,9 +4,60 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGooglePlusG, faFacebookF, faGithub } from '@fortawesome/free-brands-svg-icons';
 import env from '@/constant/env';
+import { RegisterRequest, registerRequest, registerSchema } from '../../../schema/auth.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../../../components/ui/form';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useToast } from '../../../components/ui/use-toast';
+import { Input } from '../../../components/ui/input';
+import { Button } from '../../../components/ui/button';
 
 const LoginRegister: React.FC = () => {
   const [isActive, setIsActive] = useState(false);
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  const form = useForm<RegisterRequest>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: "",
+    },
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (value: RegisterRequest) => registerRequest(value),
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+      form.reset();
+    },
+    onError: (error: any) => {
+      switch (error.response.status) {
+        case 409:
+          toast({
+            title: 'Lỗi',
+            description: 'Tên tài khoản đã tồn tại, vui lòng chọn tên khác',
+            variant: 'destructive',
+          });
+          break;
+
+        default:
+          toast({
+            title: 'Lỗi',
+            description: 'Có lỗi đã xảy ra, vui lòng thử lại sau',
+            variant: 'destructive',
+          });
+          break;
+      }
+    },
+    onSettled: () => {
+      setOpen(false);
+    },
+  });
+
+
 
   const handleRegisterClick = () => {
     setIsActive(true);
@@ -46,28 +97,69 @@ const LoginRegister: React.FC = () => {
           </form>
         </div>
 
-        <div className={`form-container sign-up absolute top-0 h-full w-1/2 p-10 transition-transform duration-600 ${isActive ? 'transform translate-x-0 opacity-0 z-10' : 'transform translate-x-full opacity-100 z-10'}`}>
-          <form className="flex flex-col items-center justify-center h-full">
-            <h1 className="font-bold text-xl mb-5">Tạo tài khoản</h1>
-            <div className="social-icons flex justify-center mb-5 space-x-2">
-              <a href="#" className="icon flex items-center justify-center w-10 h-10 p-2 border border-gray-300 rounded-full">
-                <FontAwesomeIcon icon={faGooglePlusG} />
-              </a>
-              <a href="#" className="icon flex items-center justify-center w-10 h-10 p-2 border border-gray-300 rounded-full">
-                <FontAwesomeIcon icon={faFacebookF} />
-              </a>
-              <a href={`${env.url.backend_url}/authorize/github`} className="icon p-2 border border-gray-300 rounded-full">
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-            </div>
-            <span className="text-sm mb-3">Hoặc sửa dụng tài khoản Email của bạn</span>
-            <input type="text" placeholder="Name" className="w-full px-3 py-2 mt-3 text-sm bg-gray-200 rounded" />
-            <input type="email" placeholder="Email" className="w-full px-3 py-2 mt-3 text-sm bg-gray-200 rounded" />
-            <input type="password" placeholder="Password" className="w-full px-3 py-2 mt-3 text-sm bg-gray-200 rounded" />
-            <button type="button" className="mt-4 px-6 py-2 text-sm font-semibold text-white bg-purple-600 rounded uppercase" onClick={handleLoginClick}>
-              Đăng ký
-            </button>
-          </form>
+        <div className={`form-container overflow-auto sign-up absolute top-0 h-full w-1/2 p-10 transition-transform duration-600 ${isActive ? 'transform translate-x-0 opacity-0 z-10' : 'transform translate-x-full opacity-100 z-10'}`}>
+          <h1 className="font-bold text-xl mb-5">Tạo tài khoản</h1>
+          <div className="social-icons flex justify-center mb-5 space-x-2">
+            <a href="#" className="icon flex items-center justify-center w-10 h-10 p-2 border border-gray-300 rounded-full">
+              <FontAwesomeIcon icon={faGooglePlusG} />
+            </a>
+            <a href="#" className="icon flex items-center justify-center w-10 h-10 p-2 border border-gray-300 rounded-full">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href={`${env.url.backend_url}/authorize/github`} className="icon p-2 border border-gray-300 rounded-full">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
+          <span className="text-sm mb-7">Hoặc sửa dụng tài khoản Email của bạn</span>
+          <Form {...form} >
+            <form onSubmit={form.handleSubmit((data) => mutate(data))} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className='flex flex-col gap-1'>
+                    <FormLabel>Nhập email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nhập mật khẩu</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Mật khẩu" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nhập mật khẩu</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nhập lại mật khẩu" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="w-full flex justify-end">
+                <Button type="submit" className="mt-4 px-6 py-2 text-sm font-semibold text-white bg-purple-600 rounded uppercase">đăng ký</Button>
+              </div>
+            </form>
+          </Form>
         </div>
 
         <div className={`overlay-container absolute top-0 h-full w-full flex`}>
@@ -96,3 +188,7 @@ const LoginRegister: React.FC = () => {
 };
 
 export default LoginRegister;
+function setOpen(arg0: boolean) {
+  throw new Error('Function not implemented.');
+}
+
---
+++ frontend/schema/auth.schema.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+
+const passwordStrength = (password: string) => {
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+};
+
+export const registerSchema = z.object({
+    email: z
+        .string()
+        .min(4, {
+            message: 'Tên tài khoản phải nhiều hơn 4 kí tự',
+        })
+        .max(100, { message: 'Tên tài khoản phải ít hơn 100 kí tự' }),
+
+    password: z
+        .string()
+        .min(8, {
+            message: 'Mật khẩu phải nhiều hơn 8 kí tự',
+        })
+        .max(100, { message: 'Mật khẩu phải ít hơn 100 kí tự' })
+        .refine(passwordStrength, {
+            message: 'Mật khẩu phải chứa ít nhất một chữ hoa, một chữ thường, một số và một ký tự đặc biệt',
+        }),
+
+    confirmPassword: z
+        .string()
+        .min(8, {
+            message: 'Mật khẩu phải nhiều hơn 8 kí tự',
+        })
+        .max(100, { message: 'Mật khẩu phải ít hơn 100 kí tự' })
+        .refine(passwordStrength, {
+            message: 'Mật khẩu phải chứa ít nhất một chữ hoa, một chữ thường, một số và một ký tự đặc biệt',
+        }),
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Mật khẩu và xác nhận mật khẩu phải trùng khớp",
+    path: ["confirmPassword"], // Cho biết lỗi sẽ hiển thị ở trường confirmPassword
+});
+
+export type RegisterRequest = z.infer<typeof registerSchema>;
+
+export async function registerRequest(request: RegisterRequest) {
+    return api.post('/login', request, {
+        data: request,
+    });
+}

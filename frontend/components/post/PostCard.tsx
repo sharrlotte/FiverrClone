523c773d9c05cca66d88d34f15b3b63625da4b11
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-12-28 10:06:59 +0700

    feat: increase height of PostCard and PostCardSkeleton components for better layout consistency

--- frontend/components/post/PostCard.tsx
+++ frontend/components/post/PostCard.tsx
@@ -15,7 +15,7 @@ export default function PostCard({ post: { id, title, isFavorite, user, starsCou
   images = images ?? [''];
 
   return (
-    <div className="flex flex-col gap-2 relative overflow-hidden border shadow-md hover:shadow-xl bg-white rounded-lg w-full h-[650px] min-h-[650px]">
+    <div className="flex flex-col gap-2 relative overflow-hidden border shadow-md hover:shadow-xl bg-white rounded-lg w-full h-[750px] min-h-[750px]">
       <FavoriteButton className="absolute top-1 right-1" postId={id} isFavorite={isFavorite} />
       <Link href={`/posts/${id}`}>
         <Image className="aspect-[3/2] w-full object-cover overflow-hidden" width={300} height={200} src={images[0]} alt={title} />
